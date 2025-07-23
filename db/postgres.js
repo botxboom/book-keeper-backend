12ecf44f9fddaff3d17a83ea8c3412a380d998c4
Gulshan Kumar <gulshankumar@Gulshans-MacBook-Pro.local>
2025-07-23 14:15:50 +0530

    update postgres config

--- db/postgres.js
+++ db/postgres.js
@@ -1,6 +1,13 @@
 const { Sequelize } = require("sequelize");
-const sequelize = new Sequelize("gulshankumar", "gulshan", "password", {
-  host: "localhost",
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // important for Render's SSL
+    },
+  },
 });
+
 module.exports = sequelize;
